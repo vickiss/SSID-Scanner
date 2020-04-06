3bc68803638bff2fa55e5db29e8fa3c326260b3f
vickiss <vickiss@protonmail.ch>
2020-04-06 19:38:16 +0800

    Draft

--- scan.sh
+++ scan.sh
@@ -2,9 +2,28 @@
 
 set -euxo pipefail
 
+MAX_TIMES=10
 AIRPORT=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
-
 [ ! -f "$AIRPORT" ] && echo "Apple 'airport' not found" && exit 1
 
-echo "Yes"
-"$AIRPORT" -s
+TEMP_FILE=/tmp/ssid-scan-$(date "+%Y%m%d_%H%M%S").txt
+RESULT_FILE=ssid-scan-$(date "+%Y%m%d_%H%M").txt
+
+BEST=0
+CURRENT=0
+for ((i = 1; i <= $MAX_TIMES; i++)); do
+    "$AIRPORT" -s >"$TEMP_FILE"
+    CURRENT=$(wc -l "$TEMP_FILE" | awk '{print $1}')
+    if [ "$CURRENT" -eq 0 ]; then
+        echo "#${i} - got blank result"
+        sleep 5
+        continue
+    fi
+    if [ "$CURRENT" -gt "$BEST" ]; then
+        BEST=$CURRENT
+        mv "$TEMP_FILE" "$RESULT_FILE"
+        echo "#${i} - got ${CURRENT}, best result for now"
+    else
+        echo "#${i} - got ${CURRENT}, not good enough"
+    fi
+done
